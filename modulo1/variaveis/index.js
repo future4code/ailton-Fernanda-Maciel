2d0ac92bc5cb1d88a3d16b2269e4212025e8ab75
Fernanda Tiyomi <fernanda_tiyomi@hotmail.com>
2022-05-09 17:32:58 -0300

    Atividade JS inserida

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,93 @@
+// interpretação e escrita de código
+
+
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+
+//resposta 5,10 // ACERTEI
+
+
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+//ESSE EXERCICIO NÃO ENTENDI DIREITO, ERREI NA RESPONSTA ACHEI QUE SERIA 20,10,10)
+
+// let horasDia = prompt("Quantas horas você trabalha por dia?")
+// let recebeDia = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${recebeDia/horasDia} por hora`)
+
+//ALTERADO PARA horasDia e recebeDia
+
+
+
+//Exercícios de escrita de código 
+//EXERCICIO 1 // 
+// let nome = "Fernanda";
+// let idade = 35;
+
+
+// let nome = prompt("Qual o seu nome?");
+// let idade = prompt("Qual a sua idade?");
+
+// console.log (typeof nome, typeof idade);
+// console.log (nome, idade);
+// //utilizei o typeof para imprimir na tela os tipos de variaveis, que seria string pois utilizei o let. 
+// alert(`Meu nome é ${nome} e eu tenho ${idade}anos`)
+
+// /**  NA PRIMEIRA VEZ DEU ERRO PQ ESTAV USANDO APENAS VIRGULAS
+// MAS DEPOIS PRESTEI A ATENÇÃO E FIZ COM COLCHETES*/ 
+// console.log (nome, idade)
+
+
+
+//O typeof imprime qual é a variavel, se é string, numero, null// 
+//o Alert imprime na tela 
+//O console.log imprime na tela os comandos
+
+
+//EXERCICIO 2//
+// let pergunta1 = prompt("Você gosta de sorvete de morango?");
+
+// console.log(pergunta1);
+// alert(`"Você gosta de sorvete de morango?" ${pergunta1}`);
+
+// let pergunta2 = prompt ("Você gosta de filme de terror?");
+// console.log(pergunta2);
+
+// alert(`"Você gosta de filme de terror?" ${pergunta2}`);
+
+// let pergunta3 = prompt ("você gosta de viajar?");
+// console.log(pergunta3);
+
+
+// console.log(pergunta1, pergunta2, pergunta3)
+
+// alert(`"Você gosta de viajar?" ${pergunta3}`);
+
+///////Finalizando exercicio
+
+//EXERCICIO 3
+let a = 10
+let b = 25
+let c = undefined
+
+c= b
+b= a
+a= c
+
+
+// Depois de trocados, teremos o seguinte resultado:
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10
+
+
+//GRAÇAS A DEUS CONSEGUI KKKK
