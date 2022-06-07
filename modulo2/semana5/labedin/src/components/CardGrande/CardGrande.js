c390ada6727449bac49579c8716f3c0a5c282e92
Fernanda Tiyomi <fernanda_tiyomi@hotmail.com>
2022-06-07 18:13:12 -0300

    atividade finalizada

--- modulo2/semana5/labedin/src/components/CardGrande/CardGrande.js
+++ modulo2/semana5/labedin/src/components/CardGrande/CardGrande.js
@@ -1,15 +1,40 @@
 import React from "react";
-import "./CardGrande.css";
+
+import styled from "styled-components";
+
+const BigCard = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid black;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+  height: auto;
+
+  > img {
+    width: 70px;
+    margin-right: 10px;
+    border-radius: 50%;
+  }
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+  }
+
+  > h4 {
+    margin-bottom: 15px;
+  }
+`;
 
 function CardGrande(props) {
   return (
-    <div className="bigcard-container">
+    <BigCard>
       <img src={props.imagem} />
       <div>
         <h4>{props.nome}</h4>
         <p>{props.descricao}</p>
       </div>
-    </div>
+    </BigCard>
   );
 }
 
--- modulo2/semana5/labedin/src/components/CardPequeno/CardPequeno.js
+++ modulo2/semana5/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,15 +1,41 @@
 import React from "react";
-import "./CardPequeno.css";
+
+import styled from "styled-components";
+
+const PequenoCard = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid black;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+  height: 50px;
+
+  > img {
+    width: 30px;
+    margin-right: 10px;
+    border-radius: 50%;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+  }
+
+  > h4 {
+    margin-bottom: 1px;
+  }
+`;
 
 function CardPequeno(props) {
   return (
-    <div className="pequenocard-container">
+    <PequenoCard>
       <img src={props.imagem} />
       <div>
         <h4>{props.email}</h4>
         <h4>{props.endereço}</h4>
       </div>
-    </div>
+    </PequenoCard>
   );
 }
 
