765a87a47b0fb94f41eb9aaf0c8af46fd76a626a
Fernanda Tiyomi <fernanda_tiyomi@hotmail.com>
2022-06-23 19:25:28 -0300

    projeto em andamento

---
+++ modulo2/projeto-labefy/labe-fer/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CriarPlayList from "./Component/CriarPlayList";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Labe♥Fer</h1>
+        <CriarPlayList />
+      </div>
+    );
+  }
+}
---
+++ modulo2/projeto-labefy/labe-fer/src/Component/CriarPlayList.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Axios from "axios";
+import axios from "axios";
+
+export default class CriarPlayList extends React.Component {
+  state = {
+    playList: [],
+  };
+
+  novaPlayList = (e) => {
+    this.setState({ playList: e.target.value });
+  };
+
+  creatPlayList = () => {
+    const body = {
+      name: this.state.playList,
+    };
+    axios
+      .post(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists",
+        body,
+        {
+          headers: {
+            Authorization: "fernanda-maciel-ailton",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        alert("PlayList cadastrada com sucesso :)");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Erro ao cadastrar PlayList :(");
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <label> Criar PlayList</label>
+        <input type={this.state.playList} onChange={this.novaPlayList} />
+        <button onClick={this.creatPlayList}>Criar ♥</button>
+        <p>{this.state.playList}</p>
+      </div>
+    );
+  }
+}
