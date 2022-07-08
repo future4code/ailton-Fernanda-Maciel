beffb3fe226ad4c266ed2adce3dcd59ad759c6eb
Fernanda <fernanda_tiyomi@hotmail.com>
2022-07-08 18:18:31 -0300

    atualizando

--- modulo3/projeto-astro-match/src/Components/TelaInicial.js
+++ modulo3/projeto-astro-match/src/Components/TelaInicial.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 const MiniContainer = styled.div`
   display: flex;
@@ -23,16 +24,75 @@ const MiniFooter = styled.footer`
   border-radius: 5px;
 `;
 
+const CardPerfil = styled.div`
+  text-align: center;
+`;
 export default function TelaInicial() {
+  const [perfil, setPerfil] = useState({});
+
+  useEffect(() => {
+    PegarPerfil();
+  }, []);
+
+  const PegarPerfil = () => {
+    const url =
+      "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/fernanda-maciel/person";
+
+    axios
+      .get(url)
+      .then((res) => {
+        setPerfil(res.data.profile);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
+  const SelecionarLike = () => {
+    const body = {
+      id: perfil.id,
+      choice: true,
+    };
+
+    axios
+      .post(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/fernanda-maciel/choose-person",
+        body
+      )
+      .then((res) => {
+        console.log(res);
+        PegarPerfil();
+        if (res.data.isMatch) {
+          alert("Deu Match");
+        } else {
+          alert("Não foi dessa vez, tente novamente");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <MiniContainer>
       <h1>Astro♥Match</h1>
+      <CardPerfil key={perfil.id}>
+        <h2>{perfil.name}</h2>
+        <h2>{perfil.age} anos</h2>
+        <img
+          width="300px"
+          height="300px"
+          src={perfil.photo}
+          alt={perfil.photo_alt}
+        />
+        <p>{perfil.bio}</p>
+      </CardPerfil>
+
       <MiniFooter>
-        <button>
-          {" "}
+        <button onClick={PegarPerfil}>
           <img src="https://img.icons8.com/emoji/50/000000/cross-mark-emoji.png" />
         </button>
-        <button>
+        <button onClick={SelecionarLike}>
           <img src="https://img.icons8.com/cotton/50/000000/hearts--v2.png" />
         </button>
       </MiniFooter>
--- modulo3/projeto-astro-match/src/Components/TelaMach.js
+++ modulo3/projeto-astro-match/src/Components/TelaMach.js
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -16,9 +17,58 @@ const Container = styled.div`
 `;
 
 export default function TelaMach() {
+  const [listaMatch, setListaMatch] = useState([]);
+
+  useEffect(() => {
+    ListaLikes();
+  }, [listaMatch]);
+
+  const ListaLikes = () => {
+    axios
+      .get(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/fernanda-maciel/matches"
+      )
+      .then((res) => {
+        setListaMatch(res.data.matches);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const apagarLista = () => {
+    axios
+      .put(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/fernanda-maciel/clear"
+      )
+      .then((response) => {
+        console.log(response);
+        alert("Se livrou heim!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <Container>
-      <h1>TelaMatch</h1>
+      <h1>Lista ♥ Matchs</h1>
+      <div>
+        {listaMatch.map((usuario) => {
+          return (
+            <div key={usuario.id}>
+              <img
+                height="70px"
+                width="70px"
+                src={usuario.photo}
+                alt={usuario.photo_alt}
+              />
+              <span>{usuario.name}</span>
+            </div>
+          );
+        })}
+
+        <button onClick={apagarLista}> APAGAR </button>
+      </div>
     </Container>
   );
 }
