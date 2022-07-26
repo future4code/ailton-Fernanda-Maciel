45805486fb633c41d5919056d3eeb8822088833e
Fernanda <fernanda_tiyomi@hotmail.com>
2022-07-25 21:30:29 -0300

    feito novamente para treinar

---
+++ modulo3/reforco2/reforco2/src/App.js
@@ -0,0 +1,100 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const titulo = "Titulo do video ";
+
+  function reproduzindoVideo() {
+    alert("O video esta sendo reproduzido");
+  }
+
+  return (
+    <div>
+      <div class="tela-inteira">
+        <header>
+          <h1>Lab Tube</h1>
+          <input type="text" placeholder="Busca" id="campoDeBusca" />
+        </header>
+
+        <main>
+          <nav class="menu-vertical">
+            <ul>
+              <li className="botoes-meunu-vertical">Início</li>
+              <li className="botoes-meunu-vertical">Em alta</li>
+              <li className="botoes-meunu-vertical">Inscrições</li>
+              <hr />
+
+              <li className="botoes-meunu-vertical">Originais</li>
+              <li className="botoes-meunu-vertical">Histórico</li>
+            </ul>
+          </nav>
+
+          <section className="painel-de-videos">
+            <div
+              className="box-pagina-principal media1"
+              onClick="reproduzVideo()"
+            >
+              <img src="https://picsum.photos/400/400?a=1 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div
+              className="box-pagina-principal media2"
+              onClick={reproduzindoVideo}
+            >
+              <img src="https://picsum.photos/400/400?a=2 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div
+              className="box-pagina-principal media3"
+              onClick={reproduzindoVideo}
+            >
+              <img src="https://picsum.photos/400/400?a=3 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div
+              className="box-pagina-principal media4"
+              onClick={reproduzindoVideo}
+            >
+              <img src="https://picsum.photos/400/400?a=4 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div
+              className="box-pagina-principal media5"
+              onClick={reproduzindoVideo}
+            >
+              <img src="https://picsum.photos/400/400?a=5 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div
+              className="box-pagina-principal media6"
+              onClick={reproduzindoVideo}
+            >
+              <img src="https://picsum.photos/400/400?a=6 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div
+              class="box-pagina-principal media7"
+              onclick={reproduzindoVideo}
+            >
+              <img src="https://picsum.photos/400/400?a=7 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div
+              class="box-pagina-principal media8"
+              onclick={reproduzindoVideo}
+            >
+              <img src="https://picsum.photos/400/400?a=8 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+          </section>
+        </main>
+
+        <footer>
+          <h4>Oi! Eu moro no footer!</h4>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
