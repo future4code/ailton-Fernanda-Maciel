d5956ae76211e16861f46946d70b35c4da2663c8
Fernanda <fernanda_tiyomi@hotmail.com>
2022-07-27 20:33:39 -0300

    atividades do dia ok

---
+++ modulo3/reforco-estados-insta4/insta4/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import Post from "./components/Post/Post";
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+function App() {
+  return (
+    <MainContainer>
+      <Post
+        nomeUsuario={"paulinha"}
+        fotoUsuario={"https://picsum.photos/50/50"}
+        fotoPost={"https://picsum.photos/200/150"}
+      />
+
+      <Post
+        nomeUsuario={"Fernanda"}
+        fotoUsuario={"https://picsum.photos/id/237/200/300"}
+        fotoPost={"https://picsum.photos/200/150"}
+      />
+
+      <Post
+        nomeUsuario={"Vitor"}
+        fotoUsuario={"https://picsum.photos/200/300?grayscale"}
+        fotoPost={"https://picsum.photos/200/150"}
+      />
+    </MainContainer>
+  );
+}
+
+export default App;
---
+++ modulo3/reforco-estados-insta4/insta4/src/components/Post/Post.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { IconeComContador } from "../IconeComContador/IconeComContador";
+import iconeCoracaoBranco from "../../img/favorite-white.svg";
+import iconeCoracaoPreto from "../../img/favorite.svg";
+import iconeComentario from "../../img/comment_icon.svg";
+import { SecaoComentario } from "../SecaoComentario/SecaoComentario";
+
+const PostContainer = styled.div`
+  border: 1px solid gray;
+  width: 300px;
+  margin-bottom: 10px;
+`;
+
+const PostHeader = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`;
+
+const PostFooter = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+`;
+
+const UserPhoto = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+`;
+
+const PostPhoto = styled.img`
+  width: 100%;
+`;
+
+function Post(props) {
+  const [curtido, setCurtido] = useState(false);
+  const [numeroCurtidas, setNumeroCurtidas] = useState(0);
+  const [comentando, setComentando] = useState(false);
+  const [numeroComentarios, setNumeroComentarios] = useState(0);
+  const [marcacao, setMarcacao] = useState(false);
+
+  const onClickCurtida = () => {
+    console.log("Curtiu!");
+    setCurtido(!curtido);
+    setNumeroCurtidas(!curtido ? numeroCurtidas + 1 : numeroCurtidas - 1);
+  };
+
+  const onClickComentario = () => {
+    setComentando(!comentando);
+  };
+
+  const onClickMarcacao = () => {
+    console.log("Vc marcou fernanda na Foto");
+
+    setMarcacao(!marcacao ? alert("Vc marcou fernanda na Foto") : marcacao);
+  };
+
+  const aoEnviarComentario = () => {
+    setComentando(false);
+    setNumeroComentarios(numeroComentarios + 1);
+  };
+
+  let iconeCurtida;
+
+  if (curtido) {
+    iconeCurtida = iconeCoracaoPreto;
+  } else {
+    iconeCurtida = iconeCoracaoBranco;
+  }
+
+  let componenteComentario;
+
+  if (comentando) {
+    componenteComentario = <SecaoComentario aoEnviar={aoEnviarComentario} />;
+  }
+
+  return (
+    <PostContainer>
+      <PostHeader>
+        <UserPhoto src={props.fotoUsuario} alt={"Imagem do usuario"} />
+        <p>{props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={props.fotoPost} alt={"Imagem do post"} />
+
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={onClickCurtida}
+          valorContador={numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={onClickMarcacao}
+          valorContador={marcacao}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={onClickComentario}
+          valorContador={numeroComentarios}
+        />
+      </PostFooter>
+      {componenteComentario}
+    </PostContainer>
+  );
+}
+
+export default Post;
