e0d138af2837e0ebd0ac3959e233705a206d7e2d
Fernanda <fernanda_tiyomi@hotmail.com>
2022-08-21 20:53:07 -0300

    finalizando

--- modulo4/sistema-bancario/src/index.ts
+++ modulo4/sistema-bancario/src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import { constants } from "buffer";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +11,7 @@ type Transacao = {
   descricao: string;
 };
 type Conta = {
+  id: number;
   name: string;
   CPF: string;
   dataNascimento: Date;
@@ -35,7 +35,24 @@ app.post("/cadastrar-conta", (request, response) => {
       response.statusCode = 406;
       throw new Error("Idade deve ser maior que 18 anos");
     }
-    conta.push({ name, CPF, dataNascimento, saldo: 0, extrato: [] });
+    //procurar se tem o mesmo CPF cadastrado
+
+    const findCpf = conta.findIndex((conta) => conta.CPF === CPF);
+    // Verifica se o CPF já está cadastrado, se for um valor acima de -1, então já existe um cliente com o mesmo CPF
+
+    if (findCpf >= 0) {
+      response.statusCode = 409;
+      throw new Error("CPF cadastrado já existe!! ");
+    }
+
+    conta.push({
+      name,
+      CPF,
+      dataNascimento,
+      saldo: 0,
+      extrato: [],
+      id: conta.length + 1,
+    });
 
     response.status(201).send("Conta criada com sucesso");
   } catch (error: any) {
@@ -54,6 +71,26 @@ app.get("/usuarios/todos", (request, response) => {
     response.status(200).send(conta);
   } catch (error: any) {
     response.send({ message: error.message });
+  }
+});
+
+//Consultar saldo de um cliente
+
+app.get("/usuario/:cpf", (request, response) => {
+  try {
+    const cpf = request.params.cpf;
+    const cliente = conta.find((c) => c.CPF === cpf);
+    if (!cliente) {
+      response.statusCode = 404;
+      throw new Error("Cliente não encontrado!!");
+    }
+    response.status(200).send({ valor: conta[4].saldo });
+  } catch (error: any) {
+    if (response.statusCode === 200) {
+      response.status(500).send(error.message);
+    } else {
+      response.status(response.statusCode).send(error.message);
+    }
   }
 });
 
